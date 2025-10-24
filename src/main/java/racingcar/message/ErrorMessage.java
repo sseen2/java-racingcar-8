8e9a201e76bbd11ca11ba0a47591117ecba68997
sseen2 <kse10023@naver.com>
2025-10-25 03:22:42 +0900

    feat: 메세지 enum 추가

---
+++ src/main/java/racingcar/message/ErrorMessage.java
@@ -0,0 +1,18 @@
+package racingcar.message;
+
+public enum ErrorMessage {
+
+    CAR_NAME_NOT_EMPTY("자동차 이름의 길이는 1자 이상이어야 합니다."),
+    CAR_NAME_TOO_LONG("자동차 이름의 길이는 5자 이하여야 합니다."),
+    TRY_COUNT_NOT_NUMBER("시도 횟수 입력 값은 숫자여야 합니다.");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
