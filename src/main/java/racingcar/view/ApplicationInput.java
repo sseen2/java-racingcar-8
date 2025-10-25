4927095667bf94753de94ed8c39aa58e902658b5
sseen2 <kse10023@naver.com>
2025-10-25 21:11:04 +0900

    feat: view 상황별 메서드 추가

--- src/main/java/racingcar/view/ApplicationInput.java
+++ src/main/java/racingcar/view/ApplicationInput.java
@@ -1,10 +1,22 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class ApplicationInput {
 
-    public String input() {
+    private static final String CAR_NAME_SPLITTER = ",";
+
+    private String input() {
         return Console.readLine();
+    }
+
+    public List<String> inputCarName() {
+        String inputCarNames = input();
+        return List.of(inputCarNames.split(CAR_NAME_SPLITTER));
+    }
+
+    public long inputTryCount() {
+        return Long.parseLong(input());
     }
 }
--- src/main/java/racingcar/view/ApplicationOutput.java
+++ src/main/java/racingcar/view/ApplicationOutput.java
@@ -1,8 +1,26 @@
 package racingcar.view;
 
+import racingcar.view.message.SystemMessage;
+
 public class ApplicationOutput {
 
-    public void print(String message) {
+    private void print(String message) {
         System.out.println(message);
+    }
+
+    public void printInputCarName() {
+        print(SystemMessage.INPUT_CAR_NAME.getMessage());
+    }
+
+    public void printInputTryCount() {
+        print(SystemMessage.INPUT_TRY_COUNT.getMessage());
+    }
+
+    public void printRaceResult() {
+        print(SystemMessage.OUTPUT_RACE_RESULT.getMessage());
+    }
+
+    public void printWinner() {
+        print(SystemMessage.OUTPUT_RACE_WINNER.getMessage());
     }
 }
