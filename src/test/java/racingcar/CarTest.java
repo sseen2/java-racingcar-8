134ee606eea37b287918113284b09cd22bfb371d
sseen2 <kse10023@naver.com>
2025-10-25 21:44:35 +0900

    test: Car 테스트코드 수정 및 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 public class CarTest {
 
     private static final String CAR_NAME = "test";
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
 
     @Test
     @DisplayName("자동차 이름이 정상적으로 초기화")
@@ -18,14 +20,26 @@ public class CarTest {
     }
 
     @Test
-    @DisplayName("자동차 이동 성공")
-    void moveTest() {
+    @DisplayName("랜덤 생성된 값이 4 이상일 경우 자동차 전진 성공")
+    void tryMoveTest() {
         Car car = new Car(CAR_NAME);
-        car.move();
         int expectedResult = 1;
 
-        int result = car.getPosition();
+        car.tryMove(MOVING_FORWARD);
 
+        int result = car.getPosition();
+        assertThat(expectedResult).isEqualTo(result);
+    }
+
+    @Test
+    @DisplayName("랜덤 생성된 값이 3 이하인 경우 자동차 전진 실패")
+    void tryMoveTestNotMove() {
+        Car car = new Car(CAR_NAME);
+        int expectedResult = 0;
+
+        car.tryMove(STOP);
+
+        int result = car.getPosition();
         assertThat(expectedResult).isEqualTo(result);
     }
 
@@ -33,7 +47,7 @@ public class CarTest {
     @DisplayName("자동차 상태 문자열은 '이름 : 이동거리' 형식으로 반환")
     void toStringTest() {
         Car car = new Car(CAR_NAME);
-        car.move();
+        car.tryMove(MOVING_FORWARD);
         String expectedResult = "test : -";
 
         String result = car.toString();
