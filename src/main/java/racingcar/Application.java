58713926679c3b90d8a203737edf981e0c560c02
sseen2 <kse10023@naver.com>
2025-10-25 22:38:31 +0900

    feat: application 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,31 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.view.ApplicationInput;
+import racingcar.view.ApplicationOutput;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ApplicationInput inputView = new ApplicationInput();
+        ApplicationOutput outputView = new ApplicationOutput();
+
+        outputView.printInputCarName();
+        List<Car> cars = inputView.inputCarName().stream()
+                .map(Car::new)
+                .toList();
+
+        outputView.printInputTryCount();
+        long tryCount = inputView.inputTryCount();
+        outputView.printBlankLine();
+
+        Race race = new Race(cars);
+
+        outputView.printRaceResult();
+        for (int i = 0; i < tryCount; i++) {
+            outputView.printRaceResult(race.start());
+        }
+
+        outputView.printWinner(race.winner());
     }
 }
