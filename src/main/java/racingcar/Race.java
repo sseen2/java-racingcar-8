7d11def5e0cb818a2dc3842890bf7d2a045809c3
sseen2 <kse10023@naver.com>
2025-10-25 22:34:58 +0900

    feat: 자동차 경주 시작 메서드 추가

--- src/main/java/racingcar/Race.java
+++ src/main/java/racingcar/Race.java
@@ -1,12 +1,27 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Race {
+
+    private static final int RANDOM_START_NUMBER = 0;
+    private static final int RANDOM_END_NUMBER = 9;
 
     private final List<Car> cars;
 
     public Race(List<Car> cars) {
         this.cars = cars;
+    }
+
+    public List<String> start() {
+        List<String> result = new ArrayList<>();
+        for (Car car : cars) {
+            car.tryMove(Randoms.pickNumberInRange(RANDOM_START_NUMBER, RANDOM_END_NUMBER));
+            result.add(car.moveResult());
+        }
+
+        return result;
     }
 }
