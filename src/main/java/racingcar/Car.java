4ec2c9a69fc55ad0fff56e1cf9443c3964c849a1
sseen2 <kse10023@naver.com>
2025-10-27 13:07:58 +0900

    feat: 자동차 이름 예외처리 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,7 +1,11 @@
 package racingcar;
 
+import racingcar.view.message.ErrorMessage;
+
 public class Car {
 
+    private static final int NAME_MAX_LENGTH = 5;
+    private static final String NAME_PATTERN = "^[a-zA-Z가-힣0-9 ]+$";
     private static final int MOVE_CONDITION = 4;
 
     private final String name;
@@ -10,6 +14,8 @@ public class Car {
     public Car(String name) {
         this.name = name;
         this.position = 0;
+
+        validateName();
     }
 
     public void move(int randomValue) {
@@ -28,5 +34,26 @@ public class Car {
 
     public int getPosition() {
         return position;
+    }
+
+    private void validateName() {
+        validateNameLength();
+        validateNamePattern();
+    }
+
+    private void validateNameLength() {
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_NOT_EMPTY.getMessage());
+        }
+
+        if (name.length() > NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_TOO_LONG.getMessage());
+        }
+    }
+
+    private void validateNamePattern() {
+        if (!name.matches(NAME_PATTERN)) {
+            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_NOT_MATCH_PATTERN.getMessage());
+        }
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,9 +1,11 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.view.message.ErrorMessage;
 
 public class CarTest {
 
@@ -41,5 +43,35 @@ public class CarTest {
 
         int result = car.getPosition();
         assertThat(expectedResult).isEqualTo(result);
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 비어있는 경우 예외 반환")
+    void nameEmptyTest() {
+        String emptyName = "";
+
+        assertThatThrownBy(() -> new Car(emptyName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.CAR_NAME_NOT_EMPTY.getMessage());
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 5자 초과인 경우 예외 반환")
+    void nameTooLongTest() {
+        String tooLongName = "매우 긴 자동차 이름";
+
+        assertThatThrownBy(() -> new Car(tooLongName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.CAR_NAME_TOO_LONG.getMessage());
+    }
+
+    @Test
+    @DisplayName("자동차 이름이 한글/영어/숫자가 아닌 이름인 경우 예외 반환")
+    void namePatternTest() {
+        String notMatchName = "특수문자!";
+
+        assertThatThrownBy(() -> new Car(notMatchName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.CAR_NAME_NOT_MATCH_PATTERN.getMessage());
     }
 }
